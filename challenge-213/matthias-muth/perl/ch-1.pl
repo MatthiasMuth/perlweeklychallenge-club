6484bfa9dc4bfcc355b38ae8a0ae85dbd9ecb274
Matthias Muth <matthias.muth@gmx.de>
2023-04-22 00:28:20 +0200

    Modify solutions task 1 and 2. Add blog.txt.

--- challenge-213/matthias-muth/perl/ch-1.pl
+++ challenge-213/matthias-muth/perl/ch-1.pl
@@ -13,15 +13,15 @@ use warnings;
 use feature 'say';
 
 sub fun_sort {
-    return [ sort { $a % 2 <=> $b % 2 || $a <=> $b } @{$_[0]} ];
+    sort { $a % 2 <=> $b % 2 || $a <=> $b } @_;
 }
 
 use Test::More;
 use Data::Dump qw( pp );
 
 do {
-    is_deeply fun_sort( $_->{INPUT} ), $_->{EXPECTED},
-        "fun_sort( " . pp( $_->{INPUT} ) . " ) == " . pp( $_->{EXPECTED} );
+    is_deeply [ fun_sort( @{$_->{INPUT}} ) ], $_->{EXPECTED},
+        "fun_sort( " . pp( @{$_->{INPUT}} ) . " ) == " . pp( @{$_->{EXPECTED}} );
 } for (
     { INPUT => [ 1,2,3,4,5,6 ], EXPECTED => [ 2,4,6,1,3,5 ] },
     { INPUT => [ 1,2 ], EXPECTED => [ 2,1 ] },
--- challenge-213/matthias-muth/perl/ch-2.pl
+++ challenge-213/matthias-muth/perl/ch-2.pl
@@ -18,13 +18,12 @@ use List::Util qw( sum );
 sub shortest_route {
     my ( $routes, $source, $destination ) = @_;
 
-    my %connections;
-    for my $route ( @$routes ) {
-        for ( 0..$#$route ) {
-            $connections{$route->[$_]} //= [];
-            push @{$connections{$route->[$_]}},
-                $_ > 0    ? $route->[$_-1] : (),
-                $_ < $#$route ? $route->[$_+1] : ();
+    my %neighbors;
+    for my $segment ( @$routes ) {
+        for ( 0..$#$segment ) {
+            push @{$neighbors{$segment->[$_]}},
+                $_ > 0    ? $segment->[$_-1] : (),
+                $_ < $#$segment ? $segment->[$_+1] : ();
         }
     }
 
@@ -35,8 +34,8 @@ sub shortest_route {
         my $last_node = $path->[-1];
         return $path
             if $last_node == $destination;
-        if ( $connections{$last_node} ) {
-            for ( @{$connections{$last_node}} ) {
+        if ( $neighbors{$last_node} ) {
+            for ( @{$neighbors{$last_node}} ) {
                 unless ( $visited{$_} ) {
                     push @stack, [ @$path, $_ ];
                     $visited{$_} = 1;
