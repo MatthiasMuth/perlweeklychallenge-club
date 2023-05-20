ce25070f017c93b23586831d737ad923ac8f7ce9
Matthias Muth <matthias.muth@gmx.de>
2023-05-20 23:50:39 +0200

    Generate code templates.

--- download_challenge/TestExtractor.pm
+++ download_challenge/TestExtractor.pm
@@ -45,29 +45,45 @@ sub run_tests {
         exit 1;
     }
 
-    no warnings 'once';
-    my ( undef, $local_tests ) = read_task( *::DATA );
-    # vsay pp( $local_tests );
-    use warnings 'once';
+    my $local_tests;
+    ( undef, $local_tests ) = read_task( *::DATA )
+	if fileno *::DATA;
 
     my ( $task_title, $task_description ) =
         read_task( "$dir/challenge-${challenge}.txt", $task );
     vsay $task_title;
 
     my @tests = (
-        extract_tests( $local_tests ),
-	extract_tests( $task_description ),
+        $local_tests      ? extract_tests( $local_tests )      : (),
+	$task_description ? extract_tests( $task_description ) : (),
     );
     vsay pp( @tests );
 
-    ( my $test_object = lc $task_title ) =~ s/\W+/_/g;
-    my $test_sub = \&{"::$test_object"};
+    ( my $sub_name = lc $task_title ) =~ s/\W+/_/g;
+    my $sub = \&{"::$sub_name"};
 
     do {
-        is $test_sub->( @{$_->{INPUT}} ),
-	    ref $_->{OUTPUT} eq 'ARRAY' ? @{$_->{OUTPUT}} : $_->{OUTPUT},
-            "$_->{TEST}: $test_object( " . pp( @{$_->{INPUT}} ) . " ) == "
-            . pp( $_->{OUTPUT} );
+        my @input_params =
+	    @{$_->{INPUT}} == 1
+	    ? ( ref $_->{INPUT}[0] eq 'ARRAY'
+		&& ! grep( ref $_, @{$_->{INPUT}[0]} ) )
+		? @{$_->{INPUT}[0]}
+		: $_->{INPUT}[0]
+	    : @{$_->{INPUT}};
+	my $expected = $_->{OUTPUT};
+	my $description = 
+	    "$_->{TEST}: $sub_name( " . pp( @input_params ) . " ) == "
+	    . pp(
+		ref $_->{OUTPUT} eq 'ARRAY' && @{$_->{OUTPUT}} == 1
+		? @{$_->{OUTPUT}}
+		: $_->{OUTPUT} );
+	my $output =
+	    ref $_->{OUTPUT} eq 'ARRAY'
+	    ? [ $sub->( @input_params ) ]
+	    : $sub->( @input_params );
+	    
+	is $output, $expected, $description;
+
     } for @tests;
 
     done_testing;
@@ -100,13 +116,11 @@ sub read_task( $fd_or_filename, $wanted_task = undef ) {
 	$task_text .= $_;
     }
 
-    # vsay "task title:", $task_title // "undef";
-    # vsay "task text:\n", $task_text;
     return $task_title, $task_text;
 }
 
 sub extract_tests( $task_text ) {
-    vsay "extract_tests( ", pp( $task_text ), " )";
+    # vsay "extract_tests( ", pp( $task_text ), " )";
 
     # These regular expressions are used for extracting input or output
     # test data.
@@ -130,7 +144,6 @@ sub extract_tests( $task_text ) {
 	/xmsg )
     {
 	my ( $test, $input, $output) = ( $1, $2, $3 );
-	vsay "decomposed: ", pp( $test, $input, $output );
 
 	push @tests, { TEST => $test };
 
@@ -168,18 +181,12 @@ sub extract_tests( $task_text ) {
 	    while ( s/($literal)\s+($literal)/$1, $2/ ) {}
 	}
 
-	vsay "checking input <$input>";
 	while ( $input =~ / ($var_name) \s* = \s* ($data_re) /xg ) {
-	    # vsay "found: <$&>";
-	    # vsay pp( \%+ );
 	    push @{$tests[-1]{VARIABLE_NAMES}}, $1;
 	    push @{$tests[-1]{INPUT}}, eval( $+{no_paren} ? "[ $2 ]" : $2 );
         };
 
-	vsay "checking output <$output>";
 	while ( $output =~ /^\s* ($data_re) $/xg ) {
-	    vsay "found: <$&> \$1:<$1>";
-	    vsay pp( \%+ );
 	    $tests[-1]{OUTPUT} = eval( $+{no_paren} ? "[ $1 ]" : $1 );
         };
     }
