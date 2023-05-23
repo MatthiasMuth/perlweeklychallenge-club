6c8a3a3ea8bfe23812051d6a7f49b6632074c6ea
Matthias Muth <matthias.muth@gmx.de>
2023-05-24 01:09:57 +0200

    Update Challenge 215 to use TextExtractor.

--- challenge-215/matthias-muth/perl/ch-1.pl
+++ challenge-215/matthias-muth/perl/ch-1.pl
@@ -12,6 +12,9 @@ use strict;
 use warnings;
 use feature 'say';
 
+use lib '.';
+use TestExtractor;
+
 use List::Util qw( sum reduce );
 
 sub odd_one_out {
@@ -20,74 +23,4 @@ sub odd_one_out {
             @_;
 }
 
-
-use Test2::V0;
-use Data::Dump qw( pp );
-
-# The test data extraction machine:
-my @tests;
-my $test_object = "odd_one_out";
-my $test_sub = \&$test_object;
-
-while ( <DATA> ) {
-    chomp $_;
-
-    /^Example/ and do {
-        push @tests, { TEST => $_ };
-        next;
-    };
-
-    /Input: / and do {
-        /\@\w+ = \(\s*(.*?)\s*[,]?\)/ and do {
-            my @list = map { s/'(.*?)'/$1/; $_ } split /, /, $1;
-
-            push @{$tests[-1]{INPUT}}, [ @list ];
-            next;
-        };
-    };
-    /Output: (.*)/ and do {
-        push @{$tests[-1]{OUTPUT}}, $1;
-        next;
-    };
-}
-
-do {
-    is $test_sub->( @{$_->{INPUT}[0]} ), $_->{OUTPUT}[0],
-        "$_->{TEST}: $test_object( @{$_->{INPUT}[0]} ) == $_->{OUTPUT}[0]";
-} for @tests;
-
-done_testing;
-
-# { INPUT => [ 1,2,3,4,5,6 ], EXPECTED => [ 2,4,6,1,3,5 ] },
-#     { INPUT => [ 1,2 ], EXPECTED => [ 2,1 ] },
-#     { INPUT => [ 1 ], EXPECTED => [ 1 ] },
-
-__DATA__
-
-Task 1: Odd one Out
-Submitted by: Mohammad S Anwar
-
-You are given a list of words (alphabetic characters only) of same size.
-
-Write a script to remove all words not sorted alphabetically and print the number of words in the list that are not alphabetically sorted.
-
-Example 1
-
-Input: @words = ('abc', 'xyz', 'tsu')
-Output: 1
-
-The words 'abc' and 'xyz' are sorted and can't be removed.
-The word 'tsu' is not sorted and hence can be removed.
-
-Example 2
-
-Input: @words = ('rat', 'cab', 'dad')
-Output: 3
-
-None of the words in the given list are sorted.
-Therefore all three needs to be removed.
-
-Example 3
-
-Input: @words = ('x', 'y', 'z')
-Output: 0
+run_tests;
