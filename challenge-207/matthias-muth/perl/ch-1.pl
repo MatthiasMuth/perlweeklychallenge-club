9ebad5da42f7aad53fd013d29c1dff794b205cc4
Matthias Muth <matthias.muth@gmx.de>
2023-05-24 01:09:54 +0200

    Update Challenge 207 to use TextExtractor.

--- challenge-207/matthias-muth/perl/ch-1.pl
+++ challenge-207/matthias-muth/perl/ch-1.pl
@@ -10,22 +10,13 @@
 
 use strict;
 use warnings;
+use feature 'say';
 
-sub keyboard_words {
+use lib '.';
+use TestExtractor;
+
+sub keyboard_word {
     return grep /^( [qwertyuiop]* | [asdfghjkl]* | [zxcvbnm]* )$/xi, @_;
 }
 
-use Test::More;
-
-do {
-    is_deeply
-        [ keyboard_words( @{$_->{INPUT}} ) ], $_->{EXPECTED},
-        "keyboard_words( @{$_->{INPUT}} ) == ( @{$_->{EXPECTED}} )";
-} for (
-    { INPUT    => [ qw( Hello Alaska Dad Peace ) ],
-      EXPECTED => [ qw( Alaska Dad ) ] },
-    { INPUT    => [ "OMG","Bye" ],
-      EXPECTED => [] },
-);
-
-done_testing;
+run_tests;
--- challenge-207/matthias-muth/perl/ch-2.pl
+++ challenge-207/matthias-muth/perl/ch-2.pl
@@ -10,20 +10,14 @@
 
 use strict;
 use warnings;
+use feature 'say';
+
+use lib '.';
+use TestExtractor;
 
 sub h_index {
     my @sorted = sort { $b <=> $a } @_;
     return scalar grep $sorted[$_] >= 1 + $_, 0..$#sorted;
 }
 
-use Test::More;
-
-do {
-    is h_index( @{$_->{INPUT}} ), $_->{EXPECTED},
-        "h_index( @{$_->{INPUT}} ) == $_->{EXPECTED}";
-} for (
-    { INPUT => [ 10,8,5,4,3 ], EXPECTED => 4 },
-    { INPUT => [ 25,8,5,3,3 ], EXPECTED => 3 },
-);
-
-done_testing;
+run_tests;
