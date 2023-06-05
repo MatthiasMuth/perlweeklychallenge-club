7ce7ec3445a6c1aaf743f38de06bdf448f47cc9f
Matthias Muth <matthias.muth@gmx.de>
2023-06-05 14:13:46 +0200

    Challenge 220 skeletons.

---
+++ challenge-220/matthias-muth/perl/ch-2.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+#
+#       The Weekly Challenge - Perl & Raku
+#       (https://theweeklychallenge.org)
+#
+#       Challenge 220 Task 2: Squareful
+#
+#       Perl solution by Matthias Muth.
+#
+
+use strict;
+use warnings;
+use feature 'say';
+
+use lib '.';
+use TestExtractor;
+
+use List::Util qw( sum min max );
+
+sub squareful {
+    my ( @ints ) = @_;
+
+    my @results;
+
+    return @results;
+}
+
+run_tests;
