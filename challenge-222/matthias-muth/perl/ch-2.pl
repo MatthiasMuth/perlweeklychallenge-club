b5aaeea63340397fd0b9d9ca2bb950110f4ba3de
Matthias Muth <matthias.muth@gmx.de>
2023-06-20 12:44:52 +0200

    Challenge 222: Update challenge-222.txt after change on web site. Task 2 solved.

--- challenge-222/matthias-muth/perl/ch-2.pl
+++ challenge-222/matthias-muth/perl/ch-2.pl
@@ -19,39 +19,42 @@ sub last_member {
     my @ints = @_;
 
     state $indent = "";
-
     vsay $indent, "last_member( @ints ):";
     $indent .= "    ";
 
-    my $result = $ints[0]
-	if @ints == 1;
-    if ( defined $result ) {
-	vsay $indent, "returning $result";
-	substr $indent, 0, 4, "";
-	return $result;
+    while ( @ints >= 2 ) {
+	my @sorted_ints = sort { $b <=> $a } @ints;
+	my %positions;
+	push @{ $positions{ $ints[$_] } }, $_
+	    for 0..$#ints;
+	vsay $indent, "sorted_ints: ( @sorted_ints )";
+	vsay $indent, "positions: ", pp \%positions;
+
+	my ( $i, $j ) =
+	    sort { $a <=> $b }
+		map shift @{$positions{$_}},
+		    @sorted_ints[ 0, 1 ];
+	print $indent, "pick $ints[$i] and $ints[$j],",
+	    " we remove both"
+	    if $verbose;
+
+	if ( my $diff = abs( $ints[$j] - $ints[$i] ) ) {
+	    print " and add new member $diff"
+		if $verbose;
+	    $ints[$j] = $diff;
+	    splice @ints, $i, 1, ();
+	}
+	else {
+	    # Remove both $i and $ji (starting with the latter one).
+	    splice @ints, $j, 1, ();
+	    splice @ints, $i, 1, ();
+	}
+	vsay " => ( @ints )";
     }
 
-    for my $i ( 0..$#ints ) {
-	for my $j ( $i + 1 .. $#ints ) {
-	    print $indent, "pick $ints[$i] and $ints[$j], we remove both"
-		if $verbose;
-	    my @new_ints = 
-		@ints[ 0 .. $i - 1, $i + 1 .. $j - 1, $j + 1 .. $#ints ];
-	    if ( my $diff = $ints[$j] - $ints[$i] ) {
-		print " and add new member $diff"
-		    if $verbose;
-	        push @new_ints, $diff;
-	    }
-	    vsay " => ( @new_ints )";
-	    $result = last_member( @new_ints );
-	    if ( defined $result ) {
-		vsay $indent, "returning $result";
-		substr $indent, 0, 4, "";
-		return $result;
-	    }
-	}
-    }
-    $result = 0;
+    # If there is one member, we return it up the chain.
+    # If there is no member, we return 0 up the chain.
+    my $result = $ints[0] // 0;
     vsay $indent, "returning $result";
     substr $indent, 0, 4, "";
     return $result;
