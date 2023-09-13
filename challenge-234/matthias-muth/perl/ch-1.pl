04b31ba45a98f6a1955941762dd78afabb336df9
Matthias Muth <matthias.muth@gmx.de>
2023-09-13 13:52:41 +0200

    Challenge 234 Task 1. README.md skeleton.

---
+++ challenge-234/matthias-muth/perl/ch-1.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+#
+#       The Weekly Challenge - Perl & Raku
+#       (https://theweeklychallenge.org)
+#
+#       Challenge 234 Task 1: Common Characters
+#
+#       Perl solution by Matthias Muth.
+#
+#	Call this script using
+#	    $ ch-1.pl
+#	or
+#	    $ ch-1.pl -v
+#	to get more output.
+#
+
+use v5.20;
+use strict;
+use warnings;
+use feature 'signatures';
+no warnings 'experimental::signatures';
+
+use lib '.';
+use TestExtractor;
+
+use List::Util qw( all );
+
+sub char_freqs( $word ) {
+    # Return an anonymous hash containing the frequencies of the characters
+    # in $word.
+    # Example:
+    #    char_freqs( "coffee" )
+    #    returns { "c" => 1, "e" => 2, "f" => 2, "o" => 1 }.
+
+    my %freqs;
+    ++$freqs{$_}
+	for split //, $word;
+    return \%freqs;
+}
+
+sub common_characters( @words ) {
+    # Prepare the number of available characters for each word.
+    my @available_chars = map char_freqs( $_ ), @words;
+    vsay pp \@available_chars;
+ 
+    # For each possible result character (cannot be more than the characters
+    # in $word[0]) check whether all words have at least one of that character
+    # available. Decrease its availability count in each word.
+    return
+	grep {
+	    my $char = $_;
+	    all { ( $available_chars[$_]{$char}-- // 0 ) > 0 } 0..$#words
+	} split //, $words[0];
+}
+
+run_tests;
---
+++ challenge-234/matthias-muth/perl/ch-2.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+#
+#       The Weekly Challenge - Perl & Raku
+#       (https://theweeklychallenge.org)
+#
+#       Challenge 234 Task 2: Unequal Triplets
+#
+#       Perl solution by Matthias Muth.
+#
+
+use v5.20;
+use strict;
+use warnings;
+use feature 'signatures';
+no warnings 'experimental::signatures';
+
+use lib '.';
+use TestExtractor;
+
+sub unequal_triplets( @ints ) {
+    my @results;
+    return @results;
+}
+
+run_tests;
