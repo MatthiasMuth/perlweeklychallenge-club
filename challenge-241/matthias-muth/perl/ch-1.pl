1df41f030d78aaec2ec5b452702e218f338bfd76
Matthias Muth <matthias.muth@gmx.de>
2023-11-11 18:58:30 +0100

    Challenge 241 style update for ch-1-pl.

--- challenge-241/matthias-muth/perl/ch-1.pl
+++ challenge-241/matthias-muth/perl/ch-1.pl
@@ -17,15 +17,12 @@ no warnings 'experimental::signatures';
 use lib '.';
 use TestExtractor;
 
-sub arithmetic_triplets( $nums_aref, $diff ) {
-    # Copy the array ref into a local array (only for easier reading).
-    my @nums = $nums_aref->@*;
-    # Create a lookup for all numbers.
-    my %nums = map { ( $_ => 1 ) } @nums;
-    # Return the number of numbers fulfilling the criteria.
-    return scalar grep
-        exists $nums{ $_ + $diff } && exists $nums{ $_ + 2 * $diff },
-        @nums;
+sub arithmetic_triplets( $nums, $diff ) {
+    my %num_lookup = map { ( $_ => 1 ) } $nums->@*;
+    return scalar grep {
+        exists $num_lookup{ $_ + $diff }
+	    && exists $num_lookup{ $_ + 2 * $diff },
+    } $nums->@*;
 }
 
 run_tests;
