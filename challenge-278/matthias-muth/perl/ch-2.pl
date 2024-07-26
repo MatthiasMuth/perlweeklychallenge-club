78c709365be17961e1ff051274ef54163b9932a3
Matthias Muth <matthias.muth@gmx.de>
2024-07-26 11:48:08 +0200

    Challenge 278 Cosmetics

--- challenge-278/matthias-muth/perl/ch-2.pl
+++ challenge-278/matthias-muth/perl/ch-2.pl
@@ -11,7 +11,7 @@
 use v5.36;
 
 sub reverse_word( $str, $char ) {
-    return $str =~ s{^.*?${char}}{ join "", sort split "", $& }er;
+    return $str =~ s{^.*?$char}{ join "", sort split "", $& }er;
 }
 
 use Test2::V0 qw( -no_srand );
