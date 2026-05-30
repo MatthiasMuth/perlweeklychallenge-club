011a48b81e7fa69b6451c6bf6f46be1adaf503f7
Matthias Muth <muthm@gmx.de>
2026-05-30 21:13:18 +0200

    Challenge 375 Task 1 and 2, README.md skeleton

--- challenge-375/matthias-muth/perl/ch-2.pl
+++ challenge-375/matthias-muth/perl/ch-2.pl
@@ -11,8 +11,12 @@
 use v5.36;
 
 sub find_k_beauty( $num, $k ) {
-    my @results;
-    return @results;
+    my $k_beauty = 0;
+    for ( 0..length( $num ) - $k ) {
+        ++$k_beauty
+            if $num % substr ( $num, $_, $k ) == 0;
+    }
+    return $k_beauty;
 }
 
 use lib qw( . ../../../lib );
