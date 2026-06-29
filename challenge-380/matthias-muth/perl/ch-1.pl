5d8c1fda165734501e41ee56a1599174e85173ab
Matthias Muth <muthm@gmx.de>
2026-06-29 13:41:44 +0200

    Challenge 380 Task 1 and 2, README.md skeleton

---
+++ challenge-380/matthias-muth/perl/ch-1.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+#
+#       The Weekly Challenge - Perl & Raku
+#       (https://theweeklychallenge.org)
+#
+#       Challenge 380 Task 1: Sum of Frequencies
+#
+#       Perl solution by Matthias Muth.
+#
+
+use v5.36;
+
+use List::Util qw( max );
+
+sub sum_of_frequencies( $str ) {
+    my ( %vowel_freqs, %other_freqs );
+    /^[aeiou]/ ? ++$vowel_freqs{$_} : ++$other_freqs{$_}
+        for fc( $str ) =~ /[a-z]/g;
+    return max( values %vowel_freqs, 0 ) + max( values %other_freqs, 0 );
+}
+
+use lib qw( . ../../../lib );
+use MultiTest;
+
+my @tests = (
+    [ "Example 1", "banana", 5 ],
+    [ "Example 2", "teestett", 7 ],
+    [ "Example 3", "aeiouuaa", 3 ],
+    [ "Example 4", "rhythm", 2 ],
+    [ "Example 5", "x", 1 ],
+);
+
+run( "sum_of_frequencies", \@tests );
+
+__END__
+
+# Version for publishing:
+
+use Test2::V0 qw( -no_srand );
+is sum_of_frequencies( $_->[1] ), $_->[2], $_->[0]
+    for @tests;
+done_testing;
---
+++ challenge-380/matthias-muth/perl/ch-2.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+#
+#       The Weekly Challenge - Perl & Raku
+#       (https://theweeklychallenge.org)
+#
+#       Challenge 380 Task 2: Reverse Degree
+#
+#       Perl solution by Matthias Muth.
+#
+
+use v5.36;
+
+use List::Util qw( sum );
+
+sub reverse_degree( $str ) {
+    return sum(
+        map $_ * ( ord( "z" ) + 1 - ord( substr( $str, $_ - 1, 1 ) ) ),
+            1..length( $str ) );
+}
+
+use lib qw( . ../../../lib );
+use MultiTest;
+
+my @tests = (
+    [ "Example 1", "z", 1 ],
+    [ "Example 2", "a", 26 ],
+    [ "Example 3", "bbc", 147 ],
+    [ "Example 4", "racecar", 560 ],
+    [ "Example 5", "zyx", 14 ],
+);
+
+run( "reverse_degree", \@tests );
+
+__END__
+
+# Version for publishing:
+
+use Test2::V0 qw( -no_srand );
+is reverse_degree( $_->[1] ), $_->[2], $_->[0]
+    for @tests;
+done_testing;
