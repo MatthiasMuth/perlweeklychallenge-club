d2b34eb7189624267cbfb5878ef85d4f1471488e
Matthias Muth <muthm@gmx.de>
2026-07-20 17:12:31 +0200

    Challenge 381 Task 1: revert to use array for @matrix

--- challenge-381/matthias-muth/perl/ch-1.pl
+++ challenge-381/matthias-muth/perl/ch-1.pl
@@ -12,12 +12,12 @@ use v5.36;
 use builtin qw( true false );
 no warnings 'experimental::builtin';
 
-sub same_row_column( $matrix ) {
-    my $n = scalar $matrix->@*;
+sub same_row_column( @matrix ) {
+    my $n = scalar @matrix;
     my ( @row_presences, @column_presences );
-    for my $r ( keys $matrix->@* ) {
-        for my $c ( keys $matrix->[$r]->@* ) {
-            my $value = $matrix->[$r][$c];
+    for my $r ( keys @matrix ) {
+        for my $c ( keys $matrix[$r]->@* ) {
+            my $value = $matrix[$r][$c];
             return false
                 unless 0 < $value <= $n
                     && ! $row_presences[$r][$value]++
@@ -31,11 +31,11 @@ use lib qw( . ../../../lib );
 use MultiTest;
 
 my @tests = (
-    [ "Example 1", [[[1 .. 4], [2, 3, 4, 1], [3, 4, 1, 2], [4, 1, 2, 3]]], T ],
-    [ "Example 2", [[[1]]], T ],
-    [ "Example 3", [[[1, 2, 5], [5, 1, 2], [2, 5, 1]]], F ],
-    [ "Example 4", [[[1, 2, 3], [1, 2, 3], [1, 2, 3]]], F ],
-    [ "Example 5", [[[1, 2, 3], [3, 1, 2], [3, 2, 1]]], F ],
+    [ "Example 1", [[1 .. 4], [2, 3, 4, 1], [3, 4, 1, 2], [4, 1, 2, 3]], T ],
+    [ "Example 2", [[1]], T ],
+    [ "Example 3", [[1, 2, 5], [5, 1, 2], [2, 5, 1]], F ],
+    [ "Example 4", [[1, 2, 3], [1, 2, 3], [1, 2, 3]], F ],
+    [ "Example 5", [[1, 2, 3], [3, 1, 2], [3, 2, 1]], F ],
 );
 
 run( "same_row_column", \@tests );
