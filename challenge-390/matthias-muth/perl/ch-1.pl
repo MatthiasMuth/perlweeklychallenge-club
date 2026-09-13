6454f8b8ba89e1e1155c02c26f52363da8dfcc8e
Matthias Muth <muthm@gmx.de>
2026-09-13 08:03:31 +0200

    Challenge 390 Task 1 and 2, with blog post

--- challenge-390/matthias-muth/perl/ch-1.pl
+++ challenge-390/matthias-muth/perl/ch-1.pl
@@ -9,21 +9,25 @@
 #
 
 use v5.36;
+use Dsay;
 
 sub decode_string( $str ) {
-    my @results;
-    return @results;
+    while ( $str =~ s< (\d+) \[ ([[:alpha:]]*) \] >{ $2 x $1 }xeg ) {
+        # Everything is in the loop condition.
+    }
+    return $str;
 }
 
 use lib qw( . ../../../lib );
 use MultiTest;
 
 my @tests = (
-    [ "Example 1", "2[3[a]]", "aaaaaa" ],
-    [ "Example 2", "10[a]", "aaaaaaaaaa" ],
-    [ "Example 3", "a2[b]c3[d]e", "abbcddde" ],
-    [ "Example 4", "2[a2[b]c]", "abbcabbc" ],
-    [ "Example 5", "1[a]2[b3[c]]", "abcccbccc" ],
+    [ "Example 1:", "2[3[a]]", "aaaaaa" ],
+    [ "Example 2:", "10[a]", "aaaaaaaaaa" ],
+    [ "Example 3:", "a2[b]c3[d]e", "abbcddde" ],
+    [ "Example 4:", "2[a2[b]c]", "abbcabbc" ],
+    [ "Example 5:", "1[a]2[b3[c]]", "abcccbccc" ],
+    [ "Own Test 1:", "abc", "abc" ],
 );
 
 run( "decode_string", \@tests );
--- challenge-390/matthias-muth/perl/ch-2.pl
+++ challenge-390/matthias-muth/perl/ch-2.pl
@@ -10,22 +10,31 @@
 
 use v5.36;
 
+use List::Util qw( minstr );
+
 sub order_characters( $str, $k ) {
-    my @results;
-    return @results;
+    # For $k >= 2, we know we can always sort the letters completely,
+    # so let's shortcut that.
+    # For k == 1, we can only rotate the string character by character.
+    # We then return the lexicographically smallest of all rotated strings.
+    return
+        $k >= 2
+        ?  join "", sort split "", $str
+        : minstr( map substr( $str, $_ ) . substr( $str, 0, $_ ),
+            0 .. length( $str ) - 1 );
 }
 
 use lib qw( . ../../../lib );
 use MultiTest;
 
 my @tests = (
-    [ "Example 1", ["dbca", 1], "adbc" ],
-    [ "Example 2", ["geeks", 2], "eegks" ],
-    [ "Example 3", ["cbaed", 3], "abcde" ],
-    [ "Example 4", ["fedcba", 4], "abcdef" ],
-    [ "Example 5", ["perl", 1], "erlp" ],
-    [ "Example 6", ["oloolooo", 1], "looloooo" ],
-    [ "Example 7", ["oloooolo", 1], "looloooo" ],
+    [ "Example 1:", ["dbca", 1], "adbc" ],
+    [ "Example 2:", ["geeks", 2], "eegks" ],
+    [ "Example 3:", ["cbaed", 3], "abcde" ],
+    [ "Example 4:", ["fedcba", 4], "abcdef" ],
+    [ "Example 5:", ["perl", 1], "erlp" ],
+    [ "Example 6:", ["oloolooo", 1], "looloooo" ],
+    [ "Example 7:", ["oloooolo", 1], "looloooo" ],
 );
 
 run( "order_characters", \@tests );
